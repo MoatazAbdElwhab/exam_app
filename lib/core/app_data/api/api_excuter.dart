e510761a7d69cb1460630ca1b93105a5657c10ff
Moataz Abd Elwhab <moatazAbdElwhab@gmail.com>
2025-02-15 08:40:15 +0300

    .

--- lib/core/app_data/api/api_excuter.dart
+++ lib/core/app_data/api/api_excuter.dart
@@ -1,1 +1,40 @@
+import 'package:dio/dio.dart';
+import 'package:exam_app/core/app_data/api/server_error.dart';
+import 'package:exam_app/core/app_data/result.dart';
+import 'package:exam_app/core/models/error_model.dart';
 
+Future<Result<T>> executeApi<T>(Future<T> Function() apiCall) async {
+  try {
+    var result = await apiCall.call();
+    return Success(result);
+  } on DioException catch (ex) {
+    switch (ex.type) {
+      case DioExceptionType.badCertificate:
+      case DioExceptionType.connectionError:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+      case DioExceptionType.connectionTimeout:
+        {
+          return Error(NetworkError());
+        }
+      case DioExceptionType.badResponse:
+        {
+          var responseCode = ex.response?.statusCode ?? 0;
+          var errorModel = ErrorModel.fromJson(ex.response?.data);
+          if (responseCode >= 400 && responseCode < 500) {
+            return Error(ClientError(errorModel));
+          }
+          if (responseCode >= 500 && responseCode < 600) {
+            return Error(ServerError(errorModel));
+          }
+          return Error(Exception("Something went wrong"));
+        }
+      default:
+        {
+          return Error(Exception("Something went wrong"));
+        }
+    }
+  } on Exception catch (ex) {
+    return Error(ex);
+  }
+}
--- lib/core/app_data/api/server_error.dart
+++ lib/core/app_data/api/server_error.dart
@@ -1,1 +1,20 @@
+import 'package:exam_app/core/models/error_model.dart';
 
+class ServerError implements Exception {
+  ErrorModel? errorModel;
+  ServerError(this.errorModel);
+}
+
+class NetworkError implements Exception {
+  NetworkError();
+}
+
+class ClientError implements Exception {
+  ErrorModel? errorModel;
+  ClientError(this.errorModel);
+}
+
+class ServerSideError implements Exception {
+  ErrorModel? errorModel;
+  ServerSideError(this.errorModel);
+}
