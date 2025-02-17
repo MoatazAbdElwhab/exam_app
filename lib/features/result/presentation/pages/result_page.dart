238371ccf8b91f44634d145d7ef368d647aadbaa
shimaaHossni <shimaa.hossni.2015@gmail.com>
2025-02-17 23:47:28 +0300

    Result_UI

--- lib/features/result/presentation/pages/result_page.dart
+++ lib/features/result/presentation/pages/result_page.dart
@@ -1,4 +1,7 @@
 // features/result/presentation/pages/result_page.dart
+import 'package:exam_app/core/resources/color_manager.dart';
+import 'package:exam_app/core/widgets/custom_app_bar.dart';
+import 'package:exam_app/features/result/presentation/widget/result_container.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -6,6 +9,25 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Result"));
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: const CustomAppBar(
+        title: 'Result',
+        canPop: false,
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return ResultContainer(
+            context: context,
+            index: index,
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16);
+        },
+      ),
+    );
   }
 }
