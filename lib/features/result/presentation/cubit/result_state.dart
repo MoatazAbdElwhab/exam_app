02c334239ad2832d6cfddc5387cb91ef6926348d
shimaaHossni <shimaa.hossni.2015@gmail.com>
2025-02-21 20:00:24 +0300

    result_separation_folders

---
+++ lib/features/result/presentation/cubit/result_state.dart
@@ -0,0 +1,10 @@
+part of 'result_cubit.dart';
+
+sealed class ResultState extends Equatable {
+  const ResultState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ResultInitial extends ResultState {}
