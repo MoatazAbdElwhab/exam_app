c50b95285e5974f934a370a969789403a75924b6
Moataz Abd Elwhab <moatazAbdElwhab@gmail.com>
2025-02-23 08:51:26 +0300

    enhance ui explore page

--- lib/features/explore/presentation/pages/explore_page.dart
+++ lib/features/explore/presentation/pages/explore_page.dart
@@ -33,96 +33,103 @@ class _ExplorePageState extends State<ExplorePage> {
       value: exploreCubit,
       child: Scaffold(
         backgroundColor: ColorManager.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: BlocBuilder<ExploreCubit, ExploreState>(
-            buildWhen: (previous, current) {
-              if (current is GetSubjetcsFail ||
-                  current is GetSubjetcsLoading ||
-                  current is GetSubjetcsSuccess) {
-                return true;
-              }
-              return false;
-            },
-            builder: (context, state) {
-              switch (state) {
-                case GetSubjetcsLoading():
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                case GetSubjetcsFail():
-                  return Center(
-                    child: Text(state.errorMsg),
-                  );
-                case GetSubjetcsSuccess():
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //text surveys
-                      SizedBox(height: 56.h),
-                      Text(
-                        'Surveys',
-                        style: getMediumStyle(
-                          color: ColorManager.blue,
-                          fontSize: 22.sp,
-                        ),
-                      ),
-
-                      //search textfield and icon
-                      SizedBox(height: 16.h),
-
-                      TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(
-                            Icons.search,
-                            size: 24,
+        body: BlocBuilder<ExploreCubit, ExploreState>(
+          buildWhen: (previous, current) {
+            if (current is GetSubjetcsFail ||
+                current is GetSubjetcsLoading ||
+                current is GetSubjetcsSuccess) {
+              return true;
+            }
+            return false;
+          },
+          builder: (context, state) {
+            switch (state) {
+              case GetSubjetcsLoading():
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case GetSubjetcsFail():
+                return Center(
+                  child: Text(state.errorMsg),
+                );
+              case GetSubjetcsSuccess():
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //text surveys
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 56.h),
+                          Text(
+                            'Surveys',
+                            style: getMediumStyle(
+                              color: ColorManager.blue,
+                              fontSize: 22.sp,
+                            ),
                           ),
-                          hintText: 'Search',
-                          hintStyle: getMediumStyle(
-                            color: ColorManager.grey,
-                            fontSize: 14.sp,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
 
-                      //browse by subject
-                      SizedBox(height: 30.h),
-                      Text(
-                        "Browse by Subject",
-                        style: getMediumStyle(
-                          color: ColorManager.black,
-                          fontSize: 18.sp,
-                        ),
-                      ),
+                          //search textfield and icon
+                          SizedBox(height: 16.h),
 
-                      //listview of subjects
-                      SizedBox(height: 12.h),
-                      Expanded(
-                        child: ListView.builder(
-                          padding: EdgeInsets.zero,
-                          itemCount: state.subjects.length,
-                          itemBuilder: (context, index) {
-                            final subject = state.subjects[index];
-                            return GestureDetector(
-                              onTap: () => {},
-                              child: SubjectCard(
-                                iconUrl: subject.icon,
-                                title: subject.name,
+                          TextField(
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(
+                                Icons.search,
+                                size: 24,
                               ),
-                            );
-                          },
-                        ),
+                              hintText: 'Search',
+                              hintStyle: getMediumStyle(
+                                color: ColorManager.grey,
+                                fontSize: 14.sp,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+
+                          //browse by subject
+                          SizedBox(height: 30.h),
+                          Text(
+                            "Browse by Subject",
+                            style: getMediumStyle(
+                              color: ColorManager.black,
+                              fontSize: 18.sp,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  );
-                default:
-                  return const SizedBox.shrink();
-              }
-            },
-          ),
+                    ),
+
+                    //listview of subjects
+                    SizedBox(height: 12.h),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 16),
+                        itemCount: state.subjects.length,
+                        // clipBehavior: Clip.none,
+                        itemBuilder: (context, index) {
+                          final subject = state.subjects[index];
+                          return GestureDetector(
+                            onTap: () => {},
+                            child: SubjectCard(
+                              iconUrl: subject.icon,
+                              title: subject.name,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              default:
+                return const SizedBox.shrink();
+            }
+          },
         ),
       ),
     );
